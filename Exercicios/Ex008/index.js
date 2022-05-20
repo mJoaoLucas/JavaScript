37f988d16d54dcd579398e595f60229e901e6f28
bozokkexe <mjoolucas7@gmail.com>
2022-05-20 17:02:19 -0300

    af

---
+++ Exercicios/Ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60.5) {
+    console.log(`você está acima da velocidade permitida, MULTADO!`)
+}
+    console.log(`Dirija sempre com o cinto de segurança!`)
---
+++ Exercicios/Ex009/index.js
@@ -0,0 +1,9 @@
+var pais = 'ESTADOS UNIDOS'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log(`Você é Brasileiro`)
+}  else {
+    console.log (`Você é estrangeiro`)
+}
