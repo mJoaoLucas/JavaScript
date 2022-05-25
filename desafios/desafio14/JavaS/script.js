a130c4f28911ce78d10186c2db0006cbc4e3cdd3
bozokkexe <mjoolucas7@gmail.com>
2022-05-25 11:30:29 -0300

    desafio das horas concluido

---
+++ desafios/desafio14/JavaS/script.js
@@ -0,0 +1,29 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var date = new Date()
+    var hours = date.getHours()
+
+  
+
+    msg.innerHTML = `Agora são ${hours} horas`
+
+    if (hours >= 00 && hours < 12) {
+        msg.innerHTML += ', Bom dia!'
+        img.src = 'imagens/manha.png'
+         document.body.style.background = '#fec276'          
+
+    } else if (hours >= 12 && hours < 19){
+        msg.innerHTML += ', Boa tarde!'
+        img.src = 'imagens/tarde.png'
+         document.body.style.background ='#a76c69'   
+
+    } else {
+        msg.innerHTML += ', Boa noite!'
+        img.src = 'imagens/noite.png'
+        document.body.style.background ='#313946'
+
+    }
+
+}
+
